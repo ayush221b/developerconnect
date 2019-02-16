efefd5789b2625805f7d1be5dc5772d0e8a79303
Ayush Shekhar <ayush221b@gmail.com>
2019-02-16 12:39:25 +0530

    Added user validation with jwt:

--- config/passport.js
+++ config/passport.js
@@ -10,6 +10,11 @@ opts.secretOrKey = keys.secret;
 
 module.exports = passport => {
     passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
-        console.log(jwt_payload);
+        User.findById(jwt_payload.id).then(user => {
+            if (user) {
+                return done(null, user);
+            }
+            return done(none, false);
+        }).catch(err => console.log(err));
     }));
 };
