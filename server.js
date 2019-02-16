13b221bd1d65330ccfcedbb2ad6790dc0d5a54ee
Ayush Shekhar <ayush221b@gmail.com>
2019-02-16 12:24:13 +0530

    Initialized passport

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const passport = require('passport');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
@@ -14,6 +16,9 @@ app.use(bodyParser.urlencoded({
     extended: false
 }));
 app.use(bodyParser.json());
+
+// Initialize cors
+app.use(cors());
 
 //DB config
 const db = require('./config/keys').mongoURI;
@@ -29,6 +34,12 @@ mongoose
 app.get('/', (req, res) => {
     res.send('Hello');
 });
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport Config
+require('./config/passport')(passport);
 
 //Use routes
 app.use('/api/users', users);
